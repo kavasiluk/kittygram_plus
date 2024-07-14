15d8bc190905a6496cd0aaaa229ff077b84eafa2
Konstantin Vasilyuk <vasilyuk23@yandex.ru>
2024-07-15 00:18:06 +0200

    first migrations made

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
