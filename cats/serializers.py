838e1d77304effcc47fdbcdfd28a3c0b0cd630e3
Konstantin Vasilyuk <vasilyuk23@yandex.ru>
2024-07-17 21:57:24 +0200

    10/15 finished

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -12,19 +14,44 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
-    #owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementSerializer(many=True, required=False)
+    achievements = AchievementSerializer(many=True)
+    age = serializers.SerializerMethodField()
+    # Теперь поле примет только значение, упомянутое в списке CHOICES
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         if 'achievements' not in validated_data:
